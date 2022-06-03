844b0ed9a30c19fca46b05a6894f87f08a5289d6
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-06-03 07:37:28 +0000

    i

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use pyo3::prelude::*;
+use ws::{connect, CloseCode, Sender};
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -6,9 +7,37 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+#[pymethods]
+impl UgcGateway {
+    #[new]
+    fn new() -> Self {
+        UgcGateway {}
+    }
+
+    fn connect(&self, url: &str) -> PyResult<()> {
+        println!("Connecting...");
+        connect(url, |out| {
+            self.on_open(out);
+
+            move |msg| {
+                self.recv(msg);
+            }
+        }).unwrap();
+        Ok(())
+    }
+
+    fn on_open(&self, out: Sender) -> PyResult<()> {
+        println!("Connected!");
+    }
+
+    fn recv(&self, msg: &str) -> PyResult<()> {
+        println!("Received: {}", msg);
+    }
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
-fn ugc_sdk(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+fn ugc_sdk_core(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<UgcGateway>()?;
     Ok(())
 }
